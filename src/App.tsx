a6cfd52d4613ba9ca1296ac64a25775d90fde795
Aneesa Saleh <aneesa.saleh@gmail.com>
2026-02-08 17:26:14 +0100

    Set up Qur'an data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { useEffect } from 'react';
+
+import QURAN_DATA from "../data/quranData";
+
 function App() {
+
+  useEffect(() => {
+    console.log('QURAN_DATA');
+      console.log(QURAN_DATA);
+  }, [])
+
+
   return (
     <>
-      <div className="flex h-screen w-screen items-center justify-center bg-pink-300">
+      <div className="flex flex-col h-screen w-screen items-center justify-center bg-pink-300">
         <h1>Khatm by Salah Planner</h1>
+        <div className="mt-2">
+          <p>Total Ayahs: {QURAN_DATA?.ayahs?.count?.toLocaleString()}</p>
+          <p>Total Surahs: {QURAN_DATA?.surahs?.count?.toLocaleString()}</p>
+        </div>
       </div>
     </>
   )
